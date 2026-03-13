c3425622b2dafd8d82cff282c8421346f560f759
dojimori <tuyor.jynn@gmail.com>
2026-03-13 10:08:52 +0800

    style: change links styles depending on the path

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,6 +1,13 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const [path, setPath] = useState("");
+
+  useEffect(() => {
+    setPath(window.location.pathname);
+  });
+
   return (
     <>
       {" "}
@@ -22,10 +29,14 @@ function Header() {
         </div>
 
         <div className="flex gap-2 flex-col">
-          <small className="font-light text-blue-400  cursor-pointer hover:underline">
+          <small
+            className={`font-light ${path == "/" ? "text-blue-600 underline" : "text-blue-400"} cursor-pointer hover:underline `}
+          >
             <Link to="/">Flashcards</Link>
           </small>
-          <small className="font-light text-blue-400  cursor-pointer hover:underline">
+          <small
+            className={`font-light ${path == "/reading" ? "text-blue-600 underline" : "text-blue-400"} cursor-pointer hover:underline `}
+          >
             <Link to="/reading">Reading Practice</Link>
           </small>
         </div>
