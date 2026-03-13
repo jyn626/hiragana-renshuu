6d8e12387bdc4d5bbbfc93f9be3430f40c61b2f7
dojimori <tuyor.jynn@gmail.com>
2026-03-13 12:44:19 +0800

    feat: store inputs in answers array state

--- src/pages/ReadingPractice.jsx
+++ src/pages/ReadingPractice.jsx
@@ -1,9 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useEffectEvent, useState } from "react";
 import Header from "../components/common/Header";
 import Page from "../components/common/Page";
 
 function ReadingPractice() {
   const [timer, setTimer] = useState(30);
+  const [answers, setAnswers] = useState([]);
+  const [sentences, setSentences] = useState([
+    {
+      sentence: "わたしはねこです",
+      answer: "watashiwanekodesu",
+    },
+  ]);
+
+  useEffect(() => {
+    console.log(answers);
+  }, [answers]);
 
   useEffect(() => {
     if (timer == 0) return;
@@ -29,10 +40,17 @@ function ReadingPractice() {
         </div>
         <small className="mt-2">Input Romaji here</small>
         <div className="mt-4 flex flex-wrap items-center justify-center gap-1 w-full">
-          {"わたしはねこです".split("").map(() => (
+          {sentences[0].sentence.split("").map((_, index) => (
             <input
+              key={index}
               type="text"
               maxLength={3}
+              value={answers[index]}
+              onChange={(e) => {
+                const newAnswers = [...answers];
+                newAnswers[index] = e.target.value;
+                setAnswers(newAnswers);
+              }}
               className="px-2 w-[50px] border border-gray-400  outline-amber-400 "
             />
           ))}
@@ -44,7 +62,7 @@ function ReadingPractice() {
           </button>
 
           <button className=" cursor-pointer bg-[#4CAF50]/80 hover:bg-[#4CAF50]  text-center text-white px-2 text-sm  w-[200px] h-[77px]">
-            Next
+            Attempt
           </button>
         </div>
       </Page>
