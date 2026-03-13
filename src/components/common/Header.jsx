29b3f63a443047111655d4474675b40fb0532f56
dojimori <tuyor.jynn@gmail.com>
2026-03-13 10:04:26 +0800

    style: redesign links

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,23 +4,34 @@ function Header() {
   return (
     <>
       {" "}
-      <div className="mb-6 w-full px-4 py-2 border-b border-gray-200 flex items-center gap-8">
-        <img width="90" src="/public/nihongo.png" alt="" />
+      <div className="mb-6 w-full px-4 py-2 border-b border-gray-200 flex justify-between  items-center gap-8">
+        <div className="flex gap-2 items-center">
+          <img width="90" src="/public/nihongo.png" alt="" />
+          <div>
+            <h4 className="font-semibold">
+              ひらがな<small className="font-light">(hiragana)</small>{" "}
+              Flashcards
+            </h4>
+            <p className="font-medium text-gray-600">
+              ひらがなのれんしゅうへようこそ!{" "}
+              <small className="font-light">
+                (Welcome to hiragana practice!)
+              </small>
+            </p>
+          </div>
+        </div>
 
-        <div>
-          <h4 className="font-semibold">
-            ひらがな<small className="font-light">(hiragana)</small> Flashcards
-          </h4>
-          <p className="font-medium text-gray-600">
-            ひらがなのれんしゅうへようこそ!{" "}
-            <small className="font-light">
-              (Welcome to hiragana practice!)
-            </small>
-          </p>
+        <div className="flex gap-2 flex-col">
+          <small className="font-light text-blue-400  cursor-pointer hover:underline">
+            <Link to="/">Flashcards</Link>
+          </small>
+          <small className="font-light text-blue-400  cursor-pointer hover:underline">
+            <Link to="/reading">Reading Practice</Link>
+          </small>
         </div>
       </div>
       {/* exercises links */}
-      <div className="mb-6 flex gap-2 items-center">
+      {/* <div className="mb-6 flex gap-2 items-center">
         <Link
           to="/"
           className="text-sm cursor-pointer bg-[#4CAF50] text-white px-4 py-1 rounded-xs"
@@ -33,7 +44,7 @@ function Header() {
         >
           Reading Practice
         </Link>
-      </div>
+      </div> */}
     </>
   );
 }
