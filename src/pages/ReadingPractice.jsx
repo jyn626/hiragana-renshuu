650591ced8b99946ba3a0d54abf5bafe019077ea
dojimori <tuyor.jynn@gmail.com>
2026-03-13 09:54:56 +0800

    feat: reading page timer state

--- src/pages/ReadingPractice.jsx
+++ src/pages/ReadingPractice.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import Header from "../components/common/Header";
 import Page from "../components/common/Page";
 
 function ReadingPractice() {
+  const [timer, setTimer] = useState(30);
+
+  useEffect(() => {
+    if (timer == 0) return;
+    const interval = setInterval(() => {
+      setTimer((prev) => (prev > 0 ? prev - 1 : 0));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <Page>
         <Header></Header>
+
+        <div>
+          <span>{timer}</span>
+          {/* <span>00:30</span> */}
+        </div>
       </Page>
     </>
   );
