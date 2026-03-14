c600019b855b459e7df0c22590468b8dd7895e98
dojimori <tuyor.jynn@gmail.com>
2026-03-14 09:27:42 +0800

    feat: randomly generate sentences (refactor later)

--- src/pages/ReadingPractice.jsx
+++ src/pages/ReadingPractice.jsx
@@ -1,98 +1,100 @@
 import { useEffect, useEffectEvent, useState } from "react";
 import Header from "../components/common/Header";
 import Page from "../components/common/Page";
+import { useFetcher } from "react-router-dom";
 
 function ReadingPractice() {
   const [timer, setTimer] = useState(30);
   const [answers, setAnswers] = useState([]);
   const [reveal, setReveal] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(false);
-  const [sentences, setSentences] = useState([
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [shuffledSentences, setShuffledSentences] = useState([]);
+  const sentences = [
     {
-      sentence: "わたしはねこです",
+      hiragana: "わたしはねこです",
       answer: "wa ta shi wa ne ko de su",
-      reveal: "watashi wa neko desu.",
+      romaji: "watashi wa neko desu.",
     },
     {
-      sentence: "これはほんです",
+      hiragana: "これはほんです",
       answer: "ko re wa ho n de su",
-      reveal: "kore wa hon desu.",
+      romaji: "kore wa hon desu.",
     },
     {
-      sentence: "それはいぬです",
+      hiragana: "それはいぬです",
       answer: "so re wa i nu de su",
-      reveal: "sore wa inu desu.",
+      romaji: "sore wa inu desu.",
     },
     {
-      sentence: "わたしはがくせいです",
+      hiragana: "わたしはがくせいです",
       answer: "wa ta shi wa ga ku se i de su",
-      reveal: "watashi wa gakusei desu.",
+      romaji: "watashi wa gakusei desu.",
     },
     {
-      sentence: "ねこがいます",
+      hiragana: "ねこがいます",
       answer: "ne ko ga i ma su",
-      reveal: "neko ga imasu.",
+      romaji: "neko ga imasu.",
     },
     {
-      sentence: "いぬがいます",
+      hiragana: "いぬがいます",
       answer: "i nu ga i ma su",
-      reveal: "inu ga imasu.",
+      romaji: "inu ga imasu.",
     },
     {
-      sentence: "わたしはみずをのみます",
+      hiragana: "わたしはみずをのみます",
       answer: "wa ta shi wa mi zu o no mi ma su",
-      reveal: "watashi wa mizu o nomimasu.",
+      romaji: "watashi wa mizu o nomimasu.",
     },
     {
-      sentence: "ごはんをたべます",
+      hiragana: "ごはんをたべます",
       answer: "go ha n o ta be ma su",
-      reveal: "gohan o tabemasu.",
+      romaji: "gohan o tabemasu.",
     },
     {
-      sentence: "ともだちがきます",
+      hiragana: "ともだちがきます",
       answer: "to mo da chi ga ki ma su",
-      reveal: "tomodachi ga kimasu.",
+      romaji: "tomodachi ga kimasu.",
     },
     {
-      sentence: "わたしはいえにいきます",
+      hiragana: "わたしはいえにいきます",
       answer: "wa ta shi wa i e ni i ki ma su",
-      reveal: "watashi wa ie ni ikimasu.",
+      romaji: "watashi wa ie ni ikimasu.",
     },
     {
-      sentence: "ねこはかわいいです",
+      hiragana: "ねこはかわいいです",
       answer: "ne ko wa ka wa i i de su",
-      reveal: "neko wa kawaii desu.",
+      romaji: "neko wa kawaii desu.",
     },
     {
-      sentence: "いぬはおおきいです",
+      hiragana: "いぬはおおきいです",
       answer: "i nu wa o o ki i de su",
-      reveal: "inu wa ookii desu.",
+      romaji: "inu wa ookii desu.",
     },
     {
-      sentence: "わたしはほんをよみます",
+      hiragana: "わたしはほんをよみます",
       answer: "wa ta shi wa ho n o yo mi ma su",
-      reveal: "watashi wa hon o yomimasu.",
+      romaji: "watashi wa hon o yomimasu.",
     },
     {
-      sentence: "みずをのみます",
+      hiragana: "みずをのみます",
       answer: "mi zu o no mi ma su",
-      reveal: "mizu o nomimasu.",
+      romaji: "mizu o nomimasu.",
     },
     {
-      sentence: "ともだちとあそびます",
+      hiragana: "ともだちとあそびます",
       answer: "to mo da chi to a so bi ma su",
-      reveal: "tomodachi to asobimasu.",
+      romaji: "tomodachi to asobimasu.",
     },
     {
-      sentence: "わたしはにほんごをべんきょうします",
+      hiragana: "わたしはにほんごをべんきょうします",
       answer: "wa ta shi wa ni ho n go o be n kyo u shi ma su",
-      reveal: "watashi wa nihongo o benkyou shimasu.",
+      romaji: "watashi wa nihongo o benkyou shimasu.",
     },
-  ]);
+  ];
 
-  useEffect(() => {
-    console.log(answers);
-  }, [answers]);
+  // useEffect(() => {
+  //   console.log(answers);
+  // }, [answers]);
 
   useEffect(() => {
     if (timer == 0) return;
@@ -113,7 +115,7 @@ function ReadingPractice() {
 
     const answer = filterWhiteSpaces.join(" ");
 
-    const isCorrect = sentences[0].answer == answer;
+    const isCorrect = shuffledSentences[currentIndex].answer == answer;
     console.log(isCorrect);
     if (isCorrect) {
       alert("Tadashii desuu");
@@ -121,6 +123,27 @@ function ReadingPractice() {
       alert("Machigai desuu");
     }
   };
+
+  function shuffle(array) {
+    let currentIndex = array.length;
+
+    while (currentIndex != 0) {
+      const randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+
+  useEffect(() => {
+    const shuffled = shuffle(sentences);
+    setShuffledSentences(shuffled);
+  }, []);
 
   return (
     <>
@@ -134,25 +157,29 @@ function ReadingPractice() {
 
         <div className="p-2 border border-amber-200  w-full text-center">
           <h1 className="font-bold text-6xl">
-            {reveal ? sentences[0].reveal : sentences[0].sentence}
+            {reveal
+              ? shuffledSentences[currentIndex]?.romaji
+              : shuffledSentences[currentIndex]?.hiragana}
           </h1>
         </div>
         <small className="mt-2">Input Romaji here</small>
         <div className="mt-4 flex flex-wrap items-center justify-center gap-1 w-full">
-          {sentences[0].sentence.split("").map((_, index) => (
-            <input
-              key={index}
-              type="text"
-              maxLength={3}
-              value={answers[index]}
-              onChange={(e) => {
-                const newAnswers = [...answers];
-                newAnswers[index] = e.target.value;
-                setAnswers(newAnswers);
-              }}
-              className="px-2 w-[50px] border border-gray-400  outline-amber-400 "
-            />
-          ))}
+          {shuffledSentences[currentIndex]?.hiragana
+            .split("")
+            .map((_, index) => (
+              <input
+                key={index}
+                type="text"
+                maxLength={3}
+                value={answers[index]}
+                onChange={(e) => {
+                  const newAnswers = [...answers];
+                  newAnswers[index] = e.target.value;
+                  setAnswers(newAnswers);
+                }}
+                className="px-2 w-[50px] border border-gray-400  outline-amber-400 "
+              />
+            ))}
         </div>
 
         <div className="mt-6 w-full flex justify-center gap-4">
